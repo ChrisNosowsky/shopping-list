f10089b6b26afd79d2bf1281437f04b25ef1a30a
Chris <ChrisNosowsky@gmail.com>
2020-05-17 22:21:00 -0400

    initial project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppinglistComponent } from './shoppinglist/shoppinglist.component';
+import { ShoppinglisteditComponent } from './shoppinglist/shoppinglistedit/shoppinglistedit.component';
+import { RecipelistComponent } from './recipes/recipelist/recipelist.component';
+import { RecipeitemComponent } from './recipes/recipelist/recipeitem/recipeitem.component';
+import { RecipedetailComponent } from './recipes/recipedetail/recipedetail.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppinglistComponent,
+    ShoppinglisteditComponent,
+    RecipelistComponent,
+    RecipeitemComponent,
+    RecipedetailComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
