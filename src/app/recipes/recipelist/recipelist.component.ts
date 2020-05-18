576d714c318489505053c7372efcc3bd59866fb7
Chris <ChrisNosowsky@gmail.com>
2020-05-18 14:29:51 -0400

    functionaility components

--- src/app/recipes/recipelist/recipelist.component.ts
+++ src/app/recipes/recipelist/recipelist.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Recipe} from '../recipe.model';
 @Component({
   selector: 'app-recipelist',
   templateUrl: './recipelist.component.html',
   styleUrls: ['./recipelist.component.css']
 })
 export class RecipelistComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is a test', 'https://www.cscassets.com/recipes/wide_cknew/wide_32.jpg'),
+    new Recipe('A test recipe', 'This is a test', 'https://www.cscassets.com/recipes/wide_cknew/wide_32.jpg')
+  ];
 
   constructor() { }
 
--- src/app/shoppinglist/shoppinglist.component.ts
+++ src/app/shoppinglist/shoppinglist.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Ingredient} from '../shared/ingredient.model';
 @Component({
   selector: 'app-shoppinglist',
   templateUrl: './shoppinglist.component.html',
   styleUrls: ['./shoppinglist.component.css']
 })
 export class ShoppinglistComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient("Apples", 5),
+    new Ingredient("Oranges", 10)
+  ];
 
   constructor() { }
 
